706e3e19a6c8e8ed8ee7301da24c8dd5d8793535
Cathy Sullivan <cesullivan@gmail.com>
2012-02-14 20:42:26 -0800

    remove rspec junk

--- rspec/ruby/1.9.1/gems/factory_girl-2.5.0/lib/factory_girl/proxy/attributes_for.rb
+++
@@ -1,9 +0,0 @@
-module FactoryGirl
-  class Proxy #:nodoc:
-    class AttributesFor < Proxy #:nodoc:
-      def result(attribute_assigner, to_create)
-        attribute_assigner.hash
-      end
-    end
-  end
-end
--- rspec/ruby/1.9.1/gems/factory_girl-2.5.0/lib/factory_girl/version.rb
+++
@@ -1,4 +0,0 @@
-module FactoryGirl
-  VERSION = "2.5.0"
-end
-
