6866ed2ee7b4848f6ec7a84228e0fb21bd6e4afa
Cathy Sullivan <cesullivan@gmail.com>
2012-02-28 16:08:16 -0800

    Display a comment's state transition

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,20 +1,24 @@
 class Comment < ActiveRecord::Base
+  before_create :set_previous_state
   after_create :set_ticket_state
   
-  belongs_to :ticket
   belongs_to :user
   belongs_to :state
-
+  belongs_to :ticket
+  belongs_to :previous_state, :class_name => "State"
+  
   delegate :project, :to => :ticket
 
   validates :text, :presence => true
-  
-  
+    
   private
   
     def set_ticket_state
       self.ticket.state = self.state
       self.ticket.save!
     end
-  
+
+    def set_previous_state
+      self.previous_state = ticket.state
+    end      
 end
--- features/creating_comments.feature
+++ features/creating_comments.feature
@@ -37,3 +37,5 @@ Feature: Creating comments
     And I press "Create Comment"
     Then I should see "Comment has been created."
     And I should see "Open" within "#ticket .state"
+    Then show me the page
+    Then I should see "State: Open" within "#comments"
