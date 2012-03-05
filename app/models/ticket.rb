70aae969a1fce82639f97e3978c09aee5e76583f
Cathy Sullivan <cesullivan@gmail.com>
2012-03-05 14:05:58 -0800

    Add email ticket notifications

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -3,6 +3,7 @@ class Ticket < ActiveRecord::Base
     label :tag, :from => :tags, :field => :name
     label :state, :from => :state, :field => "name"
   end
+  
   belongs_to :project
   belongs_to :state
   belongs_to :user  
@@ -11,16 +12,27 @@ class Ticket < ActiveRecord::Base
   accepts_nested_attributes_for :assets
   
   has_many :comments
-  has_and_belongs_to_many :tags  
   
+  has_and_belongs_to_many :tags  
+  has_and_belongs_to_many :watchers, :join_table => "ticket_watchers",
+                                     :class_name => "User"
+
+  after_create :creator_watches_me
+
   validates :title, :presence => true
-  validates :description, :presence => true,
-                          :length => { :minimum => 10 }
+  validates :description, :presence => true, :length => { :minimum => 10 }
                           
   def tag!(tags)
     tags = tags.split(" ").map do |tag|
       Tag.find_or_create_by_name(tag)
     end
+    
     self.tags << tags
   end
+  
+  private
+  
+    def creator_watches_me
+      self.watchers << user
+    end  
 end
